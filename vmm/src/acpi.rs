e79f27cd97647858ba246ca8d94ef0e4e07ca10d
Ruoqing He <heruoqing@iscas.ac.cn>
2025-09-23 14:52:23 +0000

    stage

--- vmm/src/acpi.rs
+++ vmm/src/acpi.rs
@@ -42,6 +42,10 @@ pub const ACPI_APIC_GENERIC_DISTRIBUTOR: u8 = 12;
 pub const ACPI_APIC_GENERIC_REDISTRIBUTOR: u8 = 14;
 #[cfg(target_arch = "aarch64")]
 pub const ACPI_APIC_GENERIC_TRANSLATOR: u8 = 15;
+#[cfg(target_arch = "riscv64")]
+pub const ACPI_RISC_V_IMSIC: u8 = 0x19;
+#[cfg(target_arch = "riscv64")]
+pub const ACPI_RISC_V_APLIC: u8 = 0x1A;
 
 #[allow(dead_code)]
 #[repr(C, packed)]
